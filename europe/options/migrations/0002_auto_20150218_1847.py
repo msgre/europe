63f2cf3bf21e4d348d4a77b6a4ab0cdf5e726f7f
Michal Valoušek <michal@plovarna.cz>
2015-03-02 11:37:36 +0100

    Add option with number of result items to the existing migration

--- europe/options/migrations/0002_auto_20150218_1847.py
+++ europe/options/migrations/0002_auto_20150218_1847.py
@@ -8,6 +8,7 @@ def fill_options(apps, schema_editor):
     Option = apps.get_model("options", "Option")
     Option.objects.create(key='POCET_OTAZEK', description=u'Počet otázek pro jednu hru', value='10')
     Option.objects.create(key='CAS_NA_JEDNU_OTAZKU', description=u'Čas (ve vteřinách), které má hráč na zodpovězení jedné otázky', value='180')
+    Option.objects.create(key='POCET_VYSLEDKU', description=u'Počet výsledků, které se objeví v tabulkách nejlepších.', value='10')
 
 
 class Migration(migrations.Migration):
