5186c06e77ec18754f2323b6669defc93ad6688e
Michal Valoušek <michal@plovarna.cz>
2016-04-01 09:44:26 +0200

    Administration of quiz models

--- europe/quiz/admin.py
+++ europe/quiz/admin.py
@@ -1,3 +1,36 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from django.utils.translation import ugettext as _
+
+from .models import Category, Question
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'order', 'disabled', )
+    list_editable = ('order', 'disabled', )
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'order', 'disabled', 'icon', ),
+        }),
+        (_('Timing'), {
+            'fields': (('time_easy', 'penalty_easy'),
+                       ('time_hard', 'penalty_hard'), )
+        }),
+    )
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_display', 'country', 'difficulty', )
+    list_filter = ('difficulty', 'category', 'country', )
+    search_fields = ['question', 'country__title']
+    fields = ('country', 'difficulty', 'category', 'question', 'image', )
+
+    def question_display(self, obj):
+        if obj.question:
+            return obj.question
+        else:
+            return _('[photo]')
+    question_display.short_description = _('Question')
