daaf90de034ca62aecb9f78e3b2d196f8be4a3cd
Michal Valoušek <michal@plovarna.cz>
2016-04-12 11:16:00 +0200

    Dynamic load of SECRET_KEY

--- europe/europe/settings_credentials.py
+++ europe/europe/settings_credentials.py
@@ -1,1 +1,6 @@
-SECRET_KEY = 'ubl76$fv7lji_fqvl-2hw0=k#09aj%71n&ancqki3p((-f*u4_'
+import os
+
+try:
+    SECRET_KEY = open(os.path.join(os.path.dirname(__file__), '.secret_key')).read().strip()
+except:
+    SECRET_KEY = 'ubl76$fv7lji_fqvl-2hw0=k#09aj%71n&ancqki3p((-f*u4_'
