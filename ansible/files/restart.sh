48f60b390646114732184d3df1ad98146d82a1bf
Michal Valoušek <michal@plovarna.cz>
2016-04-12 17:10:33 +0200

    Updated Ansible playbooks

--- ansible/files/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-$HOME/europe/stop.sh
-sleep 3
-$HOME/europe/start.sh
--- ansible/files/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker kill nginx
-docker rm nginx
-docker kill europe-api
-docker rm europe-api
-docker rm europe-js
